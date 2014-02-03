4093d737974515c8f9d9e3f17e73f30b8908aa8f
crowleytimothy <timothycrowley@hotmail.co.uk>
2014-02-03 20:31:33 +0000

    .

--- tgm.php
+++ tgm.php
@@ -2,8 +2,17 @@
 require(APPPATH.'libraries/REST_Controller.php');
 class Tgm extends REST_Controller {
    function test_GET(){
-    $out = "Hello";
-    $this->response($out, 200);
+      $out = "Hello";
+      $this->response($out, 200);
+   }
+   function trends_GET(){
+      
+   }
+   function location_GET(){
+      
+   }
+   function map_GET(){
+      
    }
 }
 ?>
